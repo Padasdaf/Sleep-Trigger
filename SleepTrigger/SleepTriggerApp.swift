d14e270f3723c1cd44cc34ceadebf669928dc75f
Daniel Hu <danielhu268@gmail.com>
2025-08-12 15:35:50 -0400

    Initial Commit

---
+++ SleepTrigger/SleepTriggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTriggerApp.swift
+//  SleepTrigger
+//
+//  Created by Daniel Hu on 2025-08-12.
+//
+
+import SwiftUI
+
+@main
+struct SleepTriggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepTriggerTests/SleepTriggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleepTriggerTests.swift
+//  SleepTriggerTests
+//
+//  Created by Daniel Hu on 2025-08-12.
+//
+
+import Testing
+@testable import SleepTrigger
+
+struct SleepTriggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
